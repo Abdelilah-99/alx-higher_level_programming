2a880717378ee58d944778e385e8bc1ee00fb043
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-06-05 16:37:59 +0100

    fix

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 number = 98
-street = "Battery street"
-print(("{number} {street}").format(number=number, street=street))
+print(("{number} Battery street").format(number=number))

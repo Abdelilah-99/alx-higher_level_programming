bab878def1cbb4bc3eff5105301f3042d9076fca
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-06-06 22:35:35 +0100

    task7

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    str1 = ""
+    for i in str:
+        if 97 <= ord(i) <= 122:
+            str1 += chr(ord(i) - 32)
+        else:
+            str1 += i
+    print("{}".format(str1))

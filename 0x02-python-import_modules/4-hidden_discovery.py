f35b10015c99d8e30460edab421e14e64c59c1ef
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-06-09 01:51:46 +0100

    task0

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    names = dir(hidden_4)
-    f_names = sorted(name for name in names if not name.startswith("__"))
-    for name in f_names:
-        print(name)
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print("{:s}".format(i))

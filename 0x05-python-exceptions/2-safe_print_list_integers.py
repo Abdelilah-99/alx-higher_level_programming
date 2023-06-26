6e83e29cdd96449afb713daefdc9cdaa0c40a899
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-06-26 23:49:08 +0100

    check if integer

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for i in my_list:
-            if (isinstance(i, int) or
-                    (isinstance(i, str) and i.isdigit())) and count < x:
-                print("{:d}".format(int(i)), end="")
-                count += 1
-        print()
-        return count
-    except (ValueError, TypeError, IndexError):
-        pass
+    for i in range(my_list[x]):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return count

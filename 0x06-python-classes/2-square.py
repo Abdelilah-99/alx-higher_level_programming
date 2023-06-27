4f557a061e5bd085de077db3660f2467605d8595
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-06-27 18:08:19 +0100

    handling some error

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+define a classe with private attribute
+"""
+
+
+class Square:
+    """a classe def for a sqr"""
+    def __init__(self, size=0):
+        """initialization size: int and it's the unit length of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size <= 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

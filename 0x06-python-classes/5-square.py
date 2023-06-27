c2a6908898d062afd881fe0fdf33d8fc664ce6d2
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-06-27 23:55:45 +0100

    build square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -32,9 +32,8 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-        a = '#'
-        if self.__size > 0:
+        elif self.__size > 0:
             for i in range(self.__size):
                 for j in range(self.__size):
-                    print("{}".format(a), end="")
+                    print("{}".format('#'), end="")
                 print()

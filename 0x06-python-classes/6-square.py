860edd8845fbfdf660d6a5fdaf3f4dec72fa3005
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-06-28 01:06:16 +0100

    build square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,12 +33,12 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-        for _ in range(self.__position[1]):
-            print()
-        for _ in range(self.__size):
-            print(" " * self.__position[0], end="")
-            print("#" * self.__size)
-
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
     @property
     def position(self):
         """we retrieving the value"""

ba991b24bef611bdf6fe32b4a3eb20ba5995025e
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-07-03 16:19:33 +0100

    empty triangle class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 class Rectangle:
+    """class with 2 parameter"""
     def __init__(self, width=0, height=0):
+        """defining two parameter"""
         self.__width = width
         self.__height = height
 

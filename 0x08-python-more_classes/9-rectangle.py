3a659e47fab2c6bca288c9416dc02bcdf05530c8
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-07-03 21:11:35 +0100

    empty triangle class

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -40,7 +40,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """setter for the private instance attribute width"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -54,7 +54,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """setter for the private instance attribute height"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -65,10 +65,11 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        """returns the perimeter of the rectangle"""
+        """peri calc"""
         if self.__width == 0 or self.__height == 0:
-            return 0
-        return (self.__width * 2) + (self.__height * 2)
+            return ''
+        else:
+            return 2 * self.__width + 2 * self.__height
 
     def __str__(self):
         """fach command str() ayakghaw aliha direct had def atakhdm"""

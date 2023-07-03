b897f3a413fd2851d751e3034ac7062475f8b6ac
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-07-03 16:23:21 +0100

    empty triangle class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,20 +7,8 @@ class Rectangle:
         self.height = height
 
     @property
-    def width(self):
-        return self.__width
-
-    @property
     def height(self):
         return self.__height
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
 
     @height.setter
     def height(self, value):
@@ -29,3 +17,15 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value

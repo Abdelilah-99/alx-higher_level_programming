b1b4073ac6d2398cf7af94ba1c46e960a386a622
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-07-03 20:24:48 +0100

    empty triangle class

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -9,6 +9,23 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = '#'
 
+    @classmethod
+    def square(cls, size=0):
+        return cls(width=size, height=size)
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        area_1 = rect_1.area()
+        area_2 = rect_2.area()
+        if area_1 >= area_2:
+            return rect_1
+        elif area_1 < area_2:
+            return rect_2
+
     def __init__(self, width=0, height=0):
         """defining two parameter"""
         self.height = height
@@ -73,20 +90,3 @@ class Rectangle:
         """del an object when we cal del"""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-
-    @classmethod
-    def square(cls, size=0):
-        return cls(width=size, height=size)
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        area_1 = rect_1.area()
-        area_2 = rect_2.area()
-        if area_1 >= area_2:
-            return rect_1
-        elif area_1 < area_2:
-            return rect_2

7fe636898f598da4b3149fdab99d6fd350d9493a
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-07-03 16:46:09 +0100

    empty triangle class

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -45,4 +45,7 @@ class Rectangle:
 
     def perimeter(self):
         """peri calc"""
-        return 2*(self.__width) + 2*(self.__height)
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2*(self.__width) + 2*(self.__height)

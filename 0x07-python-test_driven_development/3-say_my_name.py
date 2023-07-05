6cfdf82f472c30f246b171a24461ab125268a7b0
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-07-05 17:48:31 +0100

    0x07

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""printing two str"""
+
+
 def say_my_name(first_name, last_name=""):
+    """dfiich lak7al"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):

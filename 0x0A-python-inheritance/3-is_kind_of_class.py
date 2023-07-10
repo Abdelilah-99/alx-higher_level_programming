098b96c12caec956cc5516d2f4d1e65f067889e8
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-07-10 19:23:42 +0100

    isinstance not type

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""checking object"""
+
+
+def is_kind_of_class(obj, a_class):
+    """checking if the object is an instance of, or if the object is an instance of a class
+    that inherited from, the specified class"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False

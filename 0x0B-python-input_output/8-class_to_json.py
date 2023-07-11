0f6785e89b2740ea22752e632176c3563dcd59a2
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-07-11 22:56:21 +0100

    file i/o

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,9 +2,8 @@
 """function that writes
  an Object to a text file,
 using a JSON representation"""
-import json
 
 
 def class_to_json(obj):
     """class to json"""
-    return json.dumps(obj.__dict__)
+    return obj.__dict__

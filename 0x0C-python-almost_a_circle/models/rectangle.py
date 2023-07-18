3518cc6b433e12e8b5643640345df1d3dcc1abc7
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-07-18 14:58:23 +0100

    almost a cercl

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""rectangle class"""
+"""Rectangle class module"""
 from .base import Base
 
 
 class Rectangle(Base):
+    """Represents a rectangle."""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a Rectangle object with width,
         height, x-coordinate, y-coordinate, and an optional id."""

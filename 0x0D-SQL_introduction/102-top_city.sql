8758cdb1304a2ab9af97b8f165d5fac65052b767
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-10-18 00:24:13 +0100

    adv

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -3,4 +3,5 @@ SELECT `city`, AVG(`value`) as `avg_temp`
 from `temperatures`
 GROUP BY `city`
 WHERE `month` == 7 or `month` == 8
-ORDER BY `avg_temp` DESC LIMIT 3;
+ORDER BY `avg_temp` DESC
+LIMIT 3;

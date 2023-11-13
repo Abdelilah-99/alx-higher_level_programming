7ad8aace1e6fabf68fd131c191a6245d184bd0e6
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-11-13 23:30:53 +0100

    10-js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+let n = parseInt(args[0], 10);
+if (isNaN(n)) {
+  console.log(1);
+} else {
+	console.log(factorial(n));
+}
+function factorial (n) {
+  if (n === 0 || n === 1) {
+	return 1;
+  } else {
+	return n * factorial(n - 1);
+  }
+}

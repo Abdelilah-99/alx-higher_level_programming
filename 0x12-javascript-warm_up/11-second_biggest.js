cb1f709e9f9028f125b0f690e0f8125d6afc51c9
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-11-14 00:39:20 +0100

    11-js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+if (args.length < 3) {
+  console.log(0);
+} else {
+  let arr = [];
+  for (let i = 0; i < args.length; i++) {
+    const toint = parseInt(args[i], 10);
+    arr = sortedfct(toint, arr);
+  }
+  console.log(arr[arr.length - 2]);
+}
+function sortedfct (toint, arr) {
+  let inserted = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (toint < arr[i]) {
+      arr.splice(i, 0, toint);
+      inserted = true;
+      break;
+    }
+  }
+  if (!inserted) {
+    arr.push(toint);
+  }
+  return arr;
+}

29e3984aed3bd7de0be5ad2b4b63dc526c4e59f7
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-11-13 15:52:45 +0100

    2-js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -6,6 +6,6 @@ if (args.length === 0) {
 	console.log('No argument');
 } else if (args.length === 1) {
 	console.log('Argument found');
-} else {
+} else if (args.length > 1) {
 	console.log('Arguments found');
 }

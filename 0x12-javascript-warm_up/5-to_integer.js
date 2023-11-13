d4caf6d0ad2063d78c903dbd2cb72d2d863bcbe3
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-11-13 22:14:08 +0100

    5-js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('process');
 const args = argv.slice(2);
-let to_int = parseInt(args, 10);
+const toInt = parseInt(args, 10);
 
-if (isNaN(to_int)) {
+if (isNaN(toInt)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + to_int);
+  console.log('My number: ' + toInt);
 }

2eec4ca82b900e932c8ee178a30c0705ccd1329b
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-11-19 15:15:44 +0100

    orm04

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Lists all states from the databaaase hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], host="localhost",
+                         port=3306, password=sys.argv[2], database=sys.argv[3])
+    cur = db.cursor()
+    exe = """SELECT *
+	FROM cities
+    ORDER BY id"""
+    cur.execute(exe)
+    results = cur.fetchall()
+    for row in results:
+        print(row)

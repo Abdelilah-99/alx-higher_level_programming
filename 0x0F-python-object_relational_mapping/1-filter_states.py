4d99b5ad535b030a546eac300f6aea1fa2635dff
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-11-19 12:38:42 +0100

    orm 01

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Lists all states from the databaaase hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    username: str = sys.argv[1]
+    password: str = sys.argv[2]
+    db_name: str = sys.argv[3]
+    host: str = "localhost"
+    port: int = 3306
+    statement: str = """SELECT * FROM states ORDER BY id"""
+
+    db = MySQLdb.connect(user=username, host=host,
+                         port=port, password=password, database=db_name)
+    cur = db.cursor()
+    exe = "SELECT * FROM states WHERE UPPER (NAME) LIKE 'N%' ORDER BY id"
+    cur.execute(exe)
+    results = cur.fetchall()
+    for row in results:
+        print(row)

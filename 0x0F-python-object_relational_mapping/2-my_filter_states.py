35df72ce41b88e1ef9ac375d469d86987f572d5f
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-11-19 13:08:49 +0100

    orm02

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
                          port=3306, password=sys.argv[2], database=sys.argv[3])
     state_name = sys.argv[4]
     cur = db.cursor()
-    exe = "SELECT * FROM states WHERE BINARY NAME = %s ORDER BY id"
+    exe = "SELECT * FROM states WHERE BINARY NAME = '%s' ORDER BY id"
     cur.execute(exe, (state_name,))
     results = cur.fetchall()
     for row in results:

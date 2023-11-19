7066e41ac1698a595fa3741328dc247147bcd261
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-11-20 00:36:09 +0100

    orm12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -20,6 +20,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session.configure(bind=engine)
     session = Session()
-    state_to_up = session.query(State).filter_by(id=2).first()
+    state_to_up = session.query(State).filter(State.id == 2).first()
     state_to_up.name = 'New Mexico'
     session.commit()

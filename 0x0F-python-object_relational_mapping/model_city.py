e31a061a33f83bd133ec6e01cc3a87b55b15eb2e
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-11-20 01:18:53 +0100

    orm14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""..."""
+"""
+This module defines a City class that represents a table `cities`
+in an SQL database.
+"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -7,10 +10,18 @@ from model_state import Base
 
 
 class City(Base):
-    """..."""
+    """
+    Represents a city in a database table.
+
+    Attributes:
+        id (int): The primary key for the table.
+        name (str): The name of the city.
+    """
+
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('state.id'))
+
+    id: int = Column(Integer, primary_key=True)
+    name: int = Column(String(128), nullable=False)
+    state_id: int = Column(Integer, ForeignKey('states.id'))
 
     state = relationship('State', backref='cities')
